04bdca72c7742af69c98699b689036256d4ac345
luigi-123 <hcoaquiraguarachi@frba.utn.edu.ar>
2026-06-01 20:07:20 +0000

    otro arreglo

--- io/src/main.c
+++ io/src/main.c
@@ -1,12 +1,16 @@
-#include <utils/hello.h>
 #include <commons/log.h>
 #include <commons/config.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <commons/temporal.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    t_log *logger = log_create("io.log","logio",0,LOG_LEVEL_INFO);
+    t_temporal* tiempo=temporal_create();
+    int64_t a =temporal_gettime(tiempo);
+    printf("tiempo Transcurrido: %d",a);
     return 0;
 }
 
